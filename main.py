a6138544ddae11d95e3c146b4f8a4adb79e70964
arturoless <173228@ids.upchiapas.edu.mx>
2020-01-21 12:04:09 -0600

    a

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import os
 
 interface = Tk()
 
-interface.filename =  filedialog.askopenfilename(initialdir = "Path Where the dialog should open first",title = 
-"Practica 1 Open CV")
+imagenPath=''
+def openFile():
+    imagenPath=filedialog.askopenfilename(initialdir = "Path Where the dialog should open first",title = "Select file")
+    
+    img = Image.open(imagenPath)
+    img = img.resize((600, 300), Image.ANTIALIAS) 
+    photoImage = ImageTk.PhotoImage(img)
+    panel = Label(interface, image = photoImage)
+    panel.grid(column=2, row=1,pady=1, columnspan=2)
+    img2 = Image.open(imagenPath).resize((600, 300), Image.ANTIALIAS) 
+    photoImage2 = ImageTk.PhotoImage(img2)
+    panel2 = Label(interface, image = photoImage2)
+    panel2.grid(column=2, row=2,pady=1, columnspan=3)
+    interface.mainloop()
 
-img = Image.open(interface.filename)
-img = img.resize((300, 150), Image.ANTIALIAS) 
-photoImage = ImageTk.PhotoImage(img)
-panel = Label(interface, image = photoImage)
-panel.grid(column=2, row=1,columnspan=3)
-img2 = Image.open(interface.filename).resize((300, 150), Image.ANTIALIAS) 
-photoImage2 = ImageTk.PhotoImage(img2)
-panel2 = Label(interface, image = photoImage2)
-panel2.grid(column=2, row=2)
-print(img.size )
 Label(interface, 
          text="Alfa").grid(row=3)
 Label(interface, 
@@ -30,8 +32,6 @@ e2 = Entry(interface)
 e1.grid(row=3, column=2)
 e2.grid(row=4, column=2)
 
-Button(interface, 
-          text='Ver cambios').grid(row=5, 
-                                                       column=1,  
-                                                       pady=4)
+Button(interface,text='Ver cambios').grid(row=5, column=2,pady=4)
+Button(interface,text='Seleccionar imagen', command=openFile).grid(row=5, column=1,pady=4)
 interface.mainloop()
